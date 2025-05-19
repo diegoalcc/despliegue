1e303237cfb1757c141ec3b758a89bde06a6ade8
diegoalcc <dacarvajalc@uqvirtual.edu.co>
2025-05-19 02:03:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from requerimiento2 import estadisticas_generales
 from requerimiento3 import analyze_category_frequencies
 from requerimiento5 import calcular_similitud_entre_abstracts
 import categorias 
-from flask import Flask, render_template
+
 
 # Título
 st.title("Despliegue Proyecto Final - Análisis de Algoritmos")
@@ -35,24 +35,6 @@ if uploaded_file:
     except Exception as e:
         st.error(f"Error al procesar el archivo JSON: {e}")
 
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    # Lista de imágenes que se encuentran en static/img/
-    imagenes = [
-        "3_Herramienta_frecuencia.png",
-        "3_Actitudes_frecuencia.png",
-        "3_Conceptos Computacionales_frecuencia.png",
-        "3_Diseño de Investigación_frecuencia.png",
-        "3_Estrategia_frecuencia.png",
-        "3_Habilidades_frecuencia.png",
-        "3_Herramienta de Evaluación_frecuencia.png"
-    ]
-    return render_template("index.html", imagenes=imagenes)
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
 # Requerimiento 3: Frecuencia de categorías y nube de palabras
 st.header("Frecuencia de categorías y Nube de Palabras (Requerimiento 3)")
